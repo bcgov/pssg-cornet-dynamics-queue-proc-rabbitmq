b9b5d857d162d0048aae7e4abc3927c7d22c4f0f
ChrisHoban <49213216+ChrisHoban@users.noreply.github.com>
2020-02-13 08:57:31 -0800

    Removing Cert Hack,
    Server may have been fixed, now breaking on the hack

--- QueueProcessingService/Util/QueueProcessorLog.cs
+++ QueueProcessingService/Util/QueueProcessorLog.cs
@@ -19,13 +19,7 @@ namespace QueueProcessingService.Util
                 .WriteTo.EventCollector(
                     splunkHost: ConfigurationManager.FetchConfig("Serilog:EventCollectorUrl"),
                     sourceType: "manual",
-                    eventCollectorToken: ConfigurationManager.FetchConfig("Serilog:Token"),
-                    #pragma warning disable CA2000 // Dispose objects before losing scope
-                           messageHandler: new HttpClientHandler()
-                           {
-                               ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
-                           }
-                    #pragma warning restore CA2000 // Dispose objects before losing scope
+                    eventCollectorToken: ConfigurationManager.FetchConfig("Serilog:Token")
                     )
                 .CreateLogger();
             Serilog.Debugging.SelfLog.Enable(Console.Error);
